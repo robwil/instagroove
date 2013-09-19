d4657f2bfa3a9ebeec890bcde2a25d111e952786
Ciara Burkett <ciaraburkett@gmail.com>
2013-09-18 20:08:18 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Instagroove::Application.config.secret_key_base = 'd26bacd9c8efeeb3506cce4f8cf6f63e409d03727106ed5c22d0436145222c50b8558b5959dfc3e831ce11723abe12468843f0fdd665d9f7ba7510168306bbc7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Instagroove::Application.config.session_store :cookie_store, key: '_instagroove_session'
